b3c7c5dbd114c6759bb3eba1b5a968cb3b14207e
Gorka <gorka@aquigorka.com>
2017-07-11 09:06:28 +0200

    Adds spaces removes tabs

--- src/index.js
+++ src/index.js
@@ -2,34 +2,35 @@ import yo from 'yo-yo'
 
 // state store
 const state = {
-    version: '0.0.1',
-    count: 1,
+  version: '0.0.1',
+  count: 1,
 }
 
 // components
 const Version = ({ version }) => yo`<div>Version: ${version}</div>`
 const Component = ({ state, update }) => {
-    const onclick = () => {
-        state.count ++
-        update()
-    }
-    const { count } = state
-    return yo`<div onclick=${onclick}>Count (click on me to increment): ${count}</div>`
+  const onclick = () => {
+    state.count ++
+    update()
+  }
+  const { count } = state
+  return yo`<div onclick=${onclick}>Count (click on me to increment): ${count}</div>`
 }
 const nav = () => yo`<ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-    </ul>`
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+  </ul>`
 
 // app.js
 const App = ({ state, update }) => {
-    const { version } = state
-    return yo`<div>
-        ${nav()}
-        ${Component({ state, update })}
-        ${Version({ version })}
-    </div>`
+  const { version, count } = state
+  return yo`<div>
+    ${nav()}
+    ${Component({ state, update })}
+    ${Version({ version })}
+    <div>Count: ${count}</div>
+  </div>`
 }
 
 
