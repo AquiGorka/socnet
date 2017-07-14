843fe91d5dc4c2c8b36c00d62b18d7c079311c64
Gorka <gorka@aquigorka.com>
2017-07-14 20:58:33 +0200

    Adds console.logs to see which events get triggered in clients when data arrives

--- src/index.js
+++ src/index.js
@@ -13,26 +13,42 @@ const log = hyperlog(db, { valueEncoding: 'json' })
 //const sw = wswarm(signalhub('socnet-demo', ['http://localhost:9005/']))
 const sw = wswarm(signalhub('socnet-demo', ['https://signalhub.mafintosh.com']))
 
+const updateLog = (node) => {
+  const { value } = node
+  state.add(value)
+  update()
+}
+
 sw.on('peer', function (peer, id) {
   console.log('PEER', id)
   peer.pipe(log.replicate()).pipe(peer)
 })
-
-log.createReadStream().pipe(to.obj(function (row, enc, next) {
-  console.log('Update from swarm: ', row)
-  const { items, ...rest } = state
-  const { value } = row
-  state.add(value)
-  update()
+sw.on('data', data => console.log('sw data: ', data))
+// read the whole log
+const stream = log.createReadStream({ live: true }).pipe(to.obj(function(node, enc, next) {
+  updateLog(node)
   next()
 }))
 
+//const stream = log.createReadStream({ live: true })
+
+stream.on('add', x => console.log('stream add: ', x))
+stream.on('data', x => console.log('stream data: ', x))
+log.on('data', x => console.log('log data: ', x))
+log.on('add', x => console.log('log add: ', x))
+
+stream.on('add', () => updateLog)
+stream.on('data', () => updateLog)
+log.on('data', () => updateLog)
+log.on('add', () => updateLog)
 
 // index.js
 const root = document.body.appendChild(document.createElement('div'))
 export const update = () => yo.update(root, App({ state, onAdd: newItem => {
   //state.add(newItem)
+  console.log(log)
   log.append(newItem, (err, node) => {
+//  log.add(null, newItem, (err, node) => {
     state.add(newItem)
     update()
   })
